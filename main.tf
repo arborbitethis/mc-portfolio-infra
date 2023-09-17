1255b71e15c5820af23e62f8f360e2544c8d3ca6
arborbitethis <thew4yew@gmail.com>
2023-09-17 16:48:08 -0400

    creation of basic ecr and api gateway

--- main.tf
+++ main.tf
@@ -12,11 +12,32 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_s3_bucket" "test_bucket3" {
-  bucket = "my-tf-test-bucket3"
+# resource "aws_s3_bucket" "test_bucket3" {
+#   bucket = "my-tf-test-bucket3"
 
-  tags = {
-    Name        = "terst bucket"
-    Environment = "Dev"
+#   tags = {
+#     Name        = "terst bucket"
+#     Environment = "Dev"
+#   }
+# }
+
+resource "aws_ecr_repository" "portfolio_ecr" {
+  name                 = "portfolio_ecr"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = true
   }
 }
+
+resource "aws_apigatewayv2_api" "portfolio_api_gateway" {
+  name          = "portfolio-http-api"
+  protocol_type = "HTTP"
+  cors_configuration {
+    allow_origins = ["https://www.courter.dev"]
+    allow_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST" ,"PUT"]
+    allow_headers = ["Content-Type", "Authorization", "X-Amz-Date", "X-Api-Key", "X-Amz-Security-Token"]
+  }
+}
+

7310f22911ac04590c37caf389e7a91f9ad3c36b
arborbitethis <thew4yew@gmail.com>
2023-09-17 06:59:32 -0400

    initial commit to verify aws provider setup

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_s3_bucket" "test_bucket" {
-  bucket = "my-tf-test-bucket"
+resource "aws_s3_bucket" "test_bucket2" {
+  bucket = "my-tf-test-bucket2"
 
   tags = {
     Name        = "terst bucket"

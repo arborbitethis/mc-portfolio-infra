b3bff2d27181db047557965b3aa6ed0b551dca99
arborbitethis <thew4yew@gmail.com>
2023-09-26 18:41:23 -0400

    Created user/role for s3, cloudwatch group for api gateway

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 ## TODO
 * Need a way to ensure that the ECR is populated with an image (or break out the lambda creation into the mc-portfolio-backend)
+* 
+* 
--- main.tf
+++ main.tf
@@ -58,6 +58,41 @@ resource "aws_s3_bucket_acl" "this" {
 
 
 ######################################################
+#  s3 IAM
+######################################################
+
+resource "aws_iam_user" "s3_user" {
+  name = "s3-user"
+}
+
+resource "aws_iam_access_key" "s3_user_key" {
+  user = aws_iam_user.s3_user.name
+}
+
+resource "aws_iam_user_policy" "s3_policy" {
+  name = "s3-policy"
+  user = aws_iam_user.s3_user.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket",
+        ]
+        Effect   = "Allow"
+        Resource = [
+          "${aws_s3_bucket.portfolio_s3.arn}",
+          "${aws_s3_bucket.portfolio_s3.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+######################################################
 #  Networking
 ######################################################
 
@@ -137,20 +172,11 @@ resource "aws_iam_role_policy_attachment" "portfolio_lambda_policy_attachment" {
 
 
 ######################################################
-#  Lambda 
+#  EC2 instance 
 ######################################################
-# resource "aws_lambda_function" "portfolio_lambda" {
-#   function_name    = "portfolio-lambda"
-#   filename         = "lambda_function_payload.zip"
-#   source_code_hash = filebase64sha256("lambda_function_payload.zip")
-#   handler          = "index.handler"
-#   role             = aws_iam_role.example.arn
-#   runtime          = "nodejs14.x"
-#   vpc_config {
-#     subnet_ids = [aws_subnet.example.id]
-#     security_group_ids = [aws_security_group.example.id]
-#   }
-# }
+
+
+
 
 
 ######################################################
@@ -166,4 +192,16 @@ resource "aws_apigatewayv2_api" "portfolio_api_gateway" {
     allow_headers = ["Content-Type", "Authorization", "X-Amz-Date", "X-Api-Key", "X-Amz-Security-Token"]
   }
 }
+
+######################################################
+#  Cloudwatch setup
+######################################################
+
+resource "aws_cloudwatch_log_group" "portfolio_api_gateway_log_group" {
+  name = "/aws/lambda/${aws_apigatewayv2_api.portfolio_api_gateway.name}"
+  retention_in_days = 30
+}
+
+
+
 

d11d2472cb057791ecbc57cd01f680edc7d3607b
arborbitethis <thew4yew@gmail.com>
2023-10-06 10:36:11 -0400

    Remove sqs queue output and relevant policy

--- main.tf
+++ main.tf
@@ -155,13 +155,13 @@ resource "aws_security_group" "portfolio_security_group" {
 ######################################################
 #  SQS Queue
 ######################################################
-resource "aws_sqs_queue" "image_time_analysis_queue" {
-  name = "ImageTimeAnalysisQueue"
-  delay_seconds       = 0
-  max_message_size    = 262144
-  message_retention_seconds = 345600
-  receive_wait_time_seconds = 0
-}
+# resource "aws_sqs_queue" "image_time_analysis_queue" {
+#   name = "ImageTimeAnalysisQueue"
+#   delay_seconds       = 0
+#   max_message_size    = 262144
+#   message_retention_seconds = 345600
+#   receive_wait_time_seconds = 0
+# }
 
 ######################################################
 #  Lambda 

cdd43b6368a135721bd27de5b204c643b1104d84
arborbitethis <thew4yew@gmail.com>
2023-11-11 23:30:36 -0500

    Update postgrest task definition with env vars

--- main.tf
+++ main.tf
@@ -383,6 +383,27 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   })
 }
 
+resource "aws_iam_policy" "ecs_secrets_policy" {
+  name        = "ecs-secrets-policy"
+  description = "Policy to allow ECS tasks to access Secrets Manager"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = "secretsmanager:GetSecretValue",
+        Resource = aws_secretsmanager_secret.postgres_password.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_secrets_policy_attach" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.ecs_secrets_policy.arn
+}
+
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
